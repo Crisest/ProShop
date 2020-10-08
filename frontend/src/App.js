942d366bc1eeed20fb182f534d1588178853cb49
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-10-08 11:44:12 -0400

    Rating and products working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import Footer from './components/Footer'
+import { Container  } from 'react-bootstrap'
+
 
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <React.Fragment>
+    <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </React.Fragment>
+    
   );
 }
 
