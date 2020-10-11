d9baa8d1e2f325b2734738938beb81547dbff47f
Crisest <ymurillo2@ncstudents.niagaracollege.ca>
2020-10-10 21:58:12 -0400

    laptop

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+
 
 app.get('/', (req, res) => {
     res.send('API is running')
@@ -16,4 +21,6 @@ app.get('/api/products/:id', (req, res) => {
     res.send(product)
 })
 
-app.listen(5000, console.log('Server Running on Port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} on Port ${PORT}`))
